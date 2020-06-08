fc5cdaeef9718a34834c511abbd0e66219909634
turqueti <turquetivitor@gmail.com>
2020-06-08 19:40:16 -0300

    alteracoes no script de tests para consistencia

--- .Materiais/t2/Casos_test/README.txt
+++ .Materiais/t2/Casos_test/README.txt
@@ -1,3 +1,3 @@
-Arquivo zip gerado em: 24/05/2020 18:20:16 
+Arquivo zip gerado em: 08/06/2020 19:26:00 
 Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
 Exercício: Trabalho 2
--- .Tools/tester2.sh
+++ .Tools/tester2.sh
@@ -17,6 +17,7 @@ else
     fi
 
     if mv  $pathToProg  $pastaCasosDeTeste ; then
+        cp $pastaDoTrab/.Binarios/* $pastaCasosDeTeste
         $pastaDoTrab/.Tools/autoTester.sh $pastaCasosDeTeste/programaTrab $pastaCasosDeTeste -c
     fi
 

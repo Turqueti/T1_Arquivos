a69efa7e9f7d9b84cd5c2e395709ccc550c8c69a
holondo <holondo@outlook.com>
2020-07-10 20:35:45 -0300

    started b tree

---
+++ btree.h
@@ -0,0 +1,13 @@
+#ifndef TAD_BTREE_H
+#define TAD_BTREE_H
+
+#define ORDEM 6
+
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include "Tad_Registro.h"
+
+typedef struct CHAVE_ CHAVE;
+typedef struct PAGINA_ PAGINA;
+typedef struct BTREE_ BTREE;

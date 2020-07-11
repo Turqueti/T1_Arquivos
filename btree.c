705661c9771aaf7a757eda0ac0cdba73656f75ce
holondo <holondo@outlook.com>
2020-07-10 21:29:55 -0300

    IMPLEMENTING

--- btree.c
+++ btree.c
@@ -1,4 +1,10 @@
 #include "btree.h"
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+#define ORDEM 6
+#define NAOENCONTRADO -1
 
 struct CHAVE_
 {
@@ -21,7 +27,31 @@ struct BTREE_
     int nChaves;
 };
 
-void busca_btree()
+int PesquisaBinaria (int vet[], int chave, int Tam)
 {
+     int inf = 0;
+     int sup = Tam-1;
+     int meio;
+     while (inf <= sup)
+     {
+          meio = (inf + sup)/2;
+          if (chave == vet[meio])
+               return meio;
+          if (chave < vet[meio])
+               sup = meio-1;
+          else
+               inf = meio+1;
+     }
+     return -1;   // não encontrado
+}
 
+int buscaRecursiva_btree(PAGINA *pag, int idPesquisa)
+{
+    if(pag == NULL) return NAOENCONTRADO;
+
+}
+
+int busca_btree(BTREE *indice, int idPesquisa)
+{
+    return buscaRecursiva_btree(indice->raiz, idPesquisa);
 }
